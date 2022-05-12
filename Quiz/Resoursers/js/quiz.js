d8b5db2488a123e6ef6b7c5975fd84cb42355a0f
ClownisJoke <yellohelloyay@gmail.com>
2022-05-12 11:41:25 +0200

    S

---
+++ Quiz/Resoursers/js/quiz.js
@@ -0,0 +1,86 @@
+// JavaScript Document
+function checkAnswer() {
+    
+    var poeng = 0; 
+    var a1 = document.getElementById("q1"); 
+
+    if (a1.value === "2009") {
+        alert("Correct answer on nr 1");
+        poeng = poeng + 1;
+		}
+	
+	var poeng = 0; 
+    var a2 = document.getElementById("q2"); 
+
+    if (a2.value === "Arcane") {
+        alert("Correct answer on nr 2");
+        poeng = poeng + 1;
+		}
+
+	
+	var poeng = 0; 
+    var a3 = document.getElementById("q3"); 
+
+    if (a3.value === "Riot") {
+        alert("Correct answer on nr 3");
+        poeng = poeng + 1;
+		}
+	
+	var poeng = 0;
+	var a4 = document.getElementById("q4");
+
+    if(a4.value === "8"){
+        alert("Correct answer on nr 4");
+        poeng = poeng + 1;	
+	}
+	
+	var poeng = 0; 
+    var a5 = document.getElementById("q55"); 
+
+    if (a5.value === "5") {
+        alert("Correct answer on nr 5");
+        poeng = poeng + 1;
+		}
+	
+	
+	var poeng = 0; 
+    var a6 = document.getElementById("q6"); 
+
+    if (a6.value === "Faker") {
+        alert("Correct answer on nr 6");
+        poeng = poeng + 1;
+		}
+	
+	var poeng = 0; 
+    var a7 = document.getElementById("q7"); 
+
+    if (a7.value === "Elements") {
+        alert("Correct answer on nr 7");
+        poeng = poeng + 1;
+		}
+	
+	var poeng = 0; 
+    var a8 = document.getElementById("q8"); 
+
+    if (a8.value === "Alliance") {
+        alert("Correct answer on nr 8");
+        poeng = poeng + 1;
+		}
+	
+	var poeng = 0; 
+    var a9 = document.getElementById("q9"); 
+
+    if (a9.value === "Rekkles") {
+        alert("Correct answer on nr 9");
+        poeng = poeng + 1;
+		}
+	
+	var poeng = 0; 
+    var a10 = document.getElementById("q10"); 
+
+    if (a10.value === "EDG") {
+        alert("Correct answer on nr 10");
+        poeng = poeng + 1;
+		}
+}
+
